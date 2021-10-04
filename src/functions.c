9f910e3bec1e65308f3ae30f3793022f6791b4e9
alonsorodarte7 <arodarte7@miners.utep.edu>
2021-10-04 13:09:18 -0600

    added the copy_str method and updated the completed methods found in functions.c

--- src/functions.c
+++ src/functions.c
@@ -1,48 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
-#include "history.h"
 
 int space_char(char c)
 {
-  if(c == ' ' || c == '\t' || c == '\n' || c == '\0')
+  if(c == ' ' || c == '\t')
     return 1;
   return 0;
 }
 int non_space_char(char c)
 {
-  if(c == ' ' || c == '\t' || c == '\n' || c == '\0')
+  if(c == ' ' || c == '\t')
     return 0;
   return 1;
 }
 char *word_start(char *str)
 {
   int i = 0;
-  while(space_char(*(str+i)) == 1)
+  while(space_char(*(str+i)) && *(str+i) != '\0')
     {
       i++;
     }
-  return &*(str+i);
+  return (str+i);
 }
-char *word_end(char *str)
+char *word_terminator(char *str)
 {
   int i = 0;
-  while(non_space_char(*(str+i)) == 1)
+  while(non_space_char(*(str+i)) && *(str+i) != '\0')
     {
       i++;
     }
+  return (str+i);
 }
 int count_words(char *str)
 {
   int i = 0;
-  while(non_space_char(*str+i) == 1)
+  while(*(str) != '\0')
+    {
+      str = word_start(str);
+      str = word_terminator(str);
+      i++;
+    }
+  return i;
+}
+short find_len(char *str)
+{
+  short i = 0;
+  while(str[i] != '\0')
     {
       i++;
     }
-  return *(str+i);
+  return i;
 }
-
-
-
+char *copy_str(char *inStr, short len)
+{
+  int i = 0;
+  char *str = malloc(len+1);
+  while(i < len || *(inStr+i) != '\0')
+    {
+      str[i] = inStr[i];
+      i++;
+    }
+  str[len] = '\0';
+  return str;
+}
+char **tokenize(char *str)
+{
+  
+}
 
 
 
