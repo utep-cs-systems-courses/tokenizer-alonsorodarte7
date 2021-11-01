a7ae3a04714cc3feb02d4c9f3d2c0ad542f60566
alonsorodarte7 <arodarte7@miners.utep.edu>
2021-10-31 18:34:35 -0600

    Added comments for tokenizer method. Methods and comments added for print_tokens and free_tokens

--- src/functions.c
+++ src/functions.c
@@ -55,7 +55,7 @@ short find_len(char *str)
 char *copy_str(char *inStr, short len)
 {
   int i = 0;
-  char *str = malloc(len+1);
+  char *str = (char*)malloc((len+1)*sizeof(char));
   while(i < len || *(inStr+i) != '\0')
     {
       str[i] = inStr[i];
@@ -66,10 +66,55 @@ char *copy_str(char *inStr, short len)
 }
 char **tokenize(char *str)
 {
+  int word_len = count_words(str);
+  //Reserves memory used for string
+  char **tokens=(char**)malloc(sizeof(char*)*(word_len+1));
+
+  //Initializes the start and end pointers
+  char *start = str;
+  char *end = word_terminator(str);
   
+  
+  for(int i = 0; i < word_len; i++)
+    {
+      //Initializes the pointers to the next start and end of current word
+      if(i > 0)
+	{
+	  start = word_start(end);
+	  end = word_terminator(start);
+	}
+      //Finds the length of the current word
+      int word_size = end - start;
+
+      //Reserves memory for current word in 2D array
+      tokens[i] = malloc(word_len*sizeof(char));
+
+      //Populates array with the current word
+      for(int j = 0;j < word_size;j++)
+	{
+	  tokens[i][j] = start[j];
+	}
+    }
+  //Last is 0
+  tokens[word_len] = 0;
+  return tokens;
 }
-
-
-
-
-
+//Prints all tokens
+void print_tokens(char **tokens)
+{
+  for(int i = 0; tokens[i] != 0; i++)
+    {
+      printf("tokens[%i] = %s\n",i,tokens[i]);
+    }
+}
+//Frees all tokens and the vector containing them
+void free_tokens(char **tokens)
+{
+  for(int i = 0;tokens[i] != 0; i++)
+    {
+      //Frees each word of tokens
+      free(tokens[i]);
+    }
+  //Frees the overall space of tokens
+  free(tokens);
+}
