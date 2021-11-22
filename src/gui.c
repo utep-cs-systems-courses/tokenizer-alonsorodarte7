6120ae96f4615b5218acb4a06058f3f378dcfc0b
alonsorodarte7 <arodarte7@miners.utep.edu>
2021-11-22 15:09:36 -0700

    Added fully working user interface with the ability to add to the history, quit the input loop, show full history and individual items.

--- src/gui.c
+++ src/gui.c
@@ -1,13 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
+#include "history.h"
+#define LIMIT 100
 
 void main()
 {
-  printf("> ");
-  char input = 0;
-  while(input != '\n')
+  char s[LIMIT];
+  char **tokenized,*ptr;
+  int i, c, id;
+  List *history = init_history();
+
+  while(s[0] != '@')
     {
-      input = getchar();
-      printf("%c",input);
+      s[0]=' '; 
+      i = 0;
+      printf("Type @ to quit, $# to find print history item, and ! to show history");
+
+      printf("> ");
+      char input = 0;
+      int i = 0;
+      while(i < LIMIT)
+	{
+	  input = getchar();
+	  if(input == '\n')
+	    {
+	      break;
+	    }
+	  s[i] = input;
+	  i++;
+	}
+      s[i] = '\0';
+      if(s[0] != '!' && s[0] != '$' && s[0] != '@')
+	{
+	  ptr = s;
+	  tokenized = tokenize(ptr);
+	  print_tokens(tokenized);
+	  add_history(history,ptr);
+	}
+      else if(s[0] == '!')
+	{
+	  print_history(history);
+	}
+      else if(s[0] == '$')
+	{
+	  if(s[1] != '\0')
+	    {
+	      printf("%s \n",get_history(history,s[1] - '0'));
+	    }
+	}
     }
+  /**
+  char str [] = "red hot chili peppers\0";
+  char *ptr = &str[0];
+  char **tokenized = tokenize(ptr);
+  List *root = init_history();
+  add_history(root,ptr);
+  print_history(root);
+  print_tokens(tokenized);
+  **/
 }
+
+
+
+
+
+
